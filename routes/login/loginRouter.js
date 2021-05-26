357300e9e5d3bfe693bacd592ce809a9ff7ec63e
ynla1 <9111873+ynla1@user.noreply.gitee.com>
2021-05-26 20:30:32 +0800

    https://github.com/ynla/node_item

--- routes/login/loginRouter.js
+++ routes/login/loginRouter.js
@@ -34,33 +34,6 @@ router.post('/login', async (req, res) => {
     }
 })
 
-router.post('/login', async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-
-    let sqlStr = "SELECT * FROM login_user where username=? and password = ?";
-    let result = await sqlQuery(sqlStr, [username, jiami(password)])
-    if (result.length == 0) {
-        //登录失败
-        res.render('info/info', {
-            title: "登录失败",
-            content: "用户或密码错误",
-            href: "/r/login",
-            hrefTxt: "登录页"
-        })
-
-    } else {
-        req.session.username = username;
-        res.render('info/info', {
-            title: "登录成功",
-            content: "立即跳转至后台",
-            href: "/admin1",
-            hrefTxt: "蛋糕官网"
-        })
-    }
-})
-
-
 
 
 
